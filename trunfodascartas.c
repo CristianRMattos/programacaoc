7bfc553a298c6e128921eaf7f8503bc42cb0f889
CristianRMattos <cristianrmattos@gmail.com>
2025-03-25 20:19:24 +0000

    Criei meu trunfo de cartas

--- trunfodascartas.c
+++ trunfodascartas.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+    char codigodacarta = 01;
+    int populacao = 200000;
+    int numerodepontosturisticos = 50;
+    float area = 100000;
+    float pib = 300000;
+    char estado = 'ceara';
+    char nomedacidade = 'fortaleza';
+
+
+    printf("codigodacarta: %c carta 01\n", codigodacarta);
+    printf("populacao: %d habitantes\n",populacao);
+    printf("numerodepontosturisticos: %d quantidade de pontos turisticos\n",numerodepontosturisticos);
+    printf("area: %.f km²\n", area);
+    printf("pib: %.f mil de reais\n", pib);
+    printf("estado: %c ceara\n", estado);
+    printf("nomedacidade: %c fortaleza\n", nomedacidade);
+
+
+    return 0;
+}
 
